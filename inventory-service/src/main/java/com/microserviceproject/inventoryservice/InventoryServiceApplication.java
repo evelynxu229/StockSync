1b868febfaa625a1ef380f24b44365f894ad44c8
evelynxu229 <evelynxu229@gmail.com>
2023-12-20 18:24:09 +0000

    finished api gateway configuration

--- inventory-service/src/main/java/com/microserviceproject/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/com/microserviceproject/inventoryservice/InventoryServiceApplication.java
@@ -32,7 +32,7 @@ public class InventoryServiceApplication {
 
 			Inventory inventory1 = new Inventory();
 			inventory1.setSkuCode("iphone_13_red");
-			inventory1.setQuantity(0);
+			inventory1.setQuantity(99);
 
 			inventoryRepository.save(inventory);
 			inventoryRepository.save(inventory1);
