73653d197fbeba40e06511c3e25ca7d2a84bbf54
evelynxu229 <evelynxu229@gmail.com>
2023-12-23 16:05:16 +0000

    implemented circuit breaker

--- inventory-service/src/main/java/com/microserviceproject/inventoryservice/service/InventoryService.java
+++ inventory-service/src/main/java/com/microserviceproject/inventoryservice/service/InventoryService.java
@@ -6,6 +6,8 @@ import com.microserviceproject.inventoryservice.model.Inventory;
 import com.microserviceproject.inventoryservice.repository.InventoryRepository;
 
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,12 +15,17 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class InventoryService {
 
     private final InventoryRepository inventoryRepository;
 
     @Transactional(readOnly = true)
+    @SneakyThrows
     public List<InventoryResponse> isInstock(List<String> skuCode){
+        log.info("Wait Started");
+        Thread.sleep(10000);
+        log.info("Wait Ended");
         return inventoryRepository.findBySkuCodeIn(skuCode).stream()
                 .map(inventory ->
                     InventoryResponse.builder()
